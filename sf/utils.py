3b70ed57dbaba0066ef2067d484105bf053f2311
fransua <serra.francois@gmail.com>
2024-08-30 10:58:18 +0200

    Added check for path compatibility for replicate_names

--- sf/utils.py
+++ sf/utils.py
@@ -28,6 +28,30 @@ def create_workdir(result_dir, sample, singularity_img,
     out.close()
 
 
+class InvalidPathError(ValueError):
+    """Exception raised for invalid file or directory path strings."""
+    def __init__(self, path_str, invalid_chars, message="Invalid path string."):
+        self.path_str = path_str
+        self.message = (
+            f"{message} The string '{path_str}' contains forbidden"
+            f" characters or patterns (such as any of: {invalid_chars}).")
+        super().__init__(self.message)
+
+def validate_path(path_str):
+    # define invalid characters for paths
+    invalid_chars = '<>:"/\\|? *'
+    if any(char in path_str for char in invalid_chars):
+        raise InvalidPathError(path_str, invalid_chars)
+    if path_str.startswith('-'):
+        raise InvalidPathError(path_str, "starting with a dash")
+
+    if path_str in ['.', '..']:
+        raise InvalidPathError(path_str, "being single or double do")
+
+    return path_str
+
+
+
 def transform_path_to_absolute(path):
     if isinstance(path, list) or isinstance(path, tuple):
         paths = []
